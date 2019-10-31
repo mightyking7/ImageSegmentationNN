14a5eda7f71f7f951cd90d71f0e57b7c2a832bd4
Isaac Buitrago <mightyking7@gmail.com>
2019-10-31 00:14:01 -0500

    Created UNet model

---
+++ project/UNet.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.functional as F
+from .models import *
+
+class UNet(nn.Module):
+
+    def __init__(self, channels, classes):
+        super(UNet, self).__init__()
+        self.inlayer = DoubleConv(channels, 64)
+        self.down1 = DownConv(64, 128)
+        self.down2 = DownConv(128, 256)
+        self.down3 = DownConv(256, 512)
+        self.down4 = DownConv(512, 1024)
+        self.up1 = UpConv(1024, 256)
+        self.up2 = UpConv(512, 128)
+        self.up3 = UpConv(256, 64)
+        self.up4 = UpConv(128, 64)
+        self.outlayer = OutConv(64, classes)
+
+    def foward(self, x):
+        x1 = self.inlayer(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outlayer(x)
+        return F.sigmoid(x)
+
+
